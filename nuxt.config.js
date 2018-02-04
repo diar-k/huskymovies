b3f36f3b2377e145c841e290ee0c5f79cea2b643
99diagha <diarkourehpaz@gmail.com>
2018-02-04 20:05:12 +0100

    -

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'Codic',
+    title: 'Huskymovies',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
